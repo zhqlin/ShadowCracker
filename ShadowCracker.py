a73d34eb05c1949999142790c787166a8a0ba510
Janusz Pazgier <januszp@live.com>
2016-09-25 13:34:17 -0700

    initial commit

---
+++ ShadowCracker.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from sys import argv
+import crypt, hashlib, pwd
+script, filename, user = argv
+#crypt, is a module built into python which is a one-way hash function based upon a modified DES #algorithm, which will help crack UNIX paswords with a dictionary
+
+def crackPass(cryptPass,username, filename):
+	wordList = open ('dictionary', 'r')
+	ctype = cryptPass.split("$")[1]
+
+
+	passwordFound = False
+	salt = cryptPass.split("$")[2]
+	insalt = "$" + ctype + "$" + salt + "$"
+	for word in wordList.readlines():
+		word = word.strip()
+		word = word.strip('\n') #Makes sure that blank space is removed
+		if len(word) == 0:
+			continue
+		cryptWord = crypt.crypt(word,insalt)
+		if (cryptWord == cryptPass):
+			print "Username: " + username + " Password: " + word + "\n"
+			passwordFound = True
+			break
+	if (passwordFound == False):
+		print "password not in dictionary"
+
+def main():
+	passwordFound = False
+	ShadowFile = open(filename, 'r')  #ShadowFile to scan for salts
+	for line in ShadowFile.readlines():
+		line = line.strip() #Makes sure that blank space is removed
+		if len(line) == 0:
+			continue
+		line = line.replace("\n","").split(":")
+		#print(line)
+		if line[1] not in ['x','*','!']:
+			username = line[0].strip()
+			cryptPass = line[1].strip()
+			if (username == user):
+				passwordFound = True
+				crackPass(cryptPass, username, filename)
+	if(passwordFound == False):
+		print "Use does not exist or does not have a password"
+
+if __name__ == "__main__":
+	main()
+
+#print "Here's your file %r:" % filename
---
+++ readme.txt
@@ -0,0 +1,18 @@
+				ShadowCracker
+
+Python Script that takes in a shadow file and tries to crack username with use of wordlist
+
+				Usage
+
+In order for the program to work you need to download/create a wordlist and place it in the same directory as the script. Rename the 
+dictionary to "dictionary". (In the real world you would provide a wordlist to use, but those were not the specificications by my client)
+
+
+./ShadowCracker.py [shadowFile] [username]
+
+
+
+You must have the same level of permissions as the shadow file. For example if shadow file is owned by root and can only be read by root, 
+you need to be root user.
+
+				Enjoy!!
